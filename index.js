59b0e4b7453afc33efbf8f71782f674a328200c4
Gael GR <124109462+GaonaRiveraDiegoGael@users.noreply.github.com>
2024-11-01 03:52:49 +0000

    ✅Check Point

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import http from "http";
 import path from "path";
-import { promises as fs } from 'fs';
 
 // Definimos la variable __dirname
 global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
@@ -9,7 +8,7 @@ const server = http.createServer(async (req, res) => {
   // Desestructurando "req"
   let { url, method } = req;
 
-  console.log(`📣 CLIENT-REQUEST: ${url} ${method}`);
+  console.log(`📣 CLIENT-REQUEST: ${req.url} ${req.method}`);
 
   // Enrutando peticiones
   switch (url) {
@@ -22,12 +21,31 @@ const server = http.createServer(async (req, res) => {
           <title>My App</title>
         </head>
         <body> 
-          <h1 style="color: #333">Enviar Mensaje</h1>
-          <form action="/message" method="POST">
-              <label for="message">Mensaje:</label>
-              <textarea id="message" name="message" required></textarea>
-              <button type="submit">Enviar</button>
-          </form>
+          <h1 style="color: #333">Hello from my server</h1>
+          <p style="color: #34495E">Estás en el recurso raíz.</p>
+        </body>
+      </html>
+      `);
+      res.statusCode = 200;
+      res.end();
+      console.log(`📣 Respondiendo: 200 ${url} ${method}`);
+      break;
+
+    case '/author':
+      // Petición a la ruta "/author"
+      res.setHeader('Content-Type', 'text/html');
+      let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
+      res.write(`
+      <html>
+        <head>
+          <title>My App</title>
+        </head>
+        <body style="text-align: center;">
+          <h1 style="color: #333;">&#9889; Author &#9889;</h1>
+          <p style="color: #34495E;">Iván Rivalcoba Rivas - Web Developer</p>
+          <div>
+            <img width="300px" src="${url_image}" alt="Foto Ivan Rivalcoba">
+          </div>
         </body>
       </html>
       `);
@@ -37,38 +55,16 @@ const server = http.createServer(async (req, res) => {
       break;
 
     case "/message":
-      // Verificando si es POST
+      // Verificando si es post
       if (method === "POST") {
-        // Se crea una variable para almacenar los datos entrantes del cliente
-        let body = "";
-        
-        // Se registra un manejador de eventos para la recepción de datos
-        req.on("data", (data) => {
-          body += data;
-          if (body.length > 1e6) return req.socket.destroy();
-        });
-
-        // Se registra un manejador de eventos para el término de recepción de datos
-        req.on("end", async () => {
-          // Procesa el formulario mediante URLSearchParams
-          const params = new URLSearchParams(body);
-          const parsedParams = Object.fromEntries(params);
-          
-          // Almacenamos en un archivo el mensaje
-          await fs.writeFile('message.txt', parsedParams.message);
-          console.log("📣 Archivo message.txt grabado");
-
-          // Realizamos un redireccionamiento
-          res.statusCode = 302;
-          res.setHeader('Location', '/');
-          // Se finaliza la conexión
-          return res.end();
-        });
+        // Procesa el formulario
+        res.statusCode = 200;
+        res.write("🎉 Endpoint Funcionando!!! 🎉");
       } else {
         res.statusCode = 404;
-        res.write("📣 404: Endpoint no encontrado");
-        res.end();
+        res.write("404: Endpoint no encontrado");
       }
+      res.end();
       break;
 
     default:
