64a0d81772b18e5418a27684105197a39c5a3314
Gael GR <124109462+GaonaRiveraDiegoGael@users.noreply.github.com>
2024-11-01 01:45:35 +0000

    🧪 Actividad: Favicon

--- index.js
+++ index.js
@@ -1,89 +1,114 @@
 import http from "http";
+import path from "path";
+import { promises as fs } from 'fs';
 
-const server = http.createServer((req, res) => {
-  // Desestructurando de "req"
+// Definimos la variable __dirname
+global["__dirname"] = path.dirname(new URL(import.meta.url).pathname);
+
+const server = http.createServer(async (req, res) => {
+  // Desestructurando "req"
   let { url, method } = req;
 
-  console.log(`📣 CLIENT-REQUEST: ${req.url} ${req.method}`);
+  console.log(`📣 CLIENT-REQUEST: ${url} ${method}`);
 
   // Enrutando peticiones
   switch (url) {
     case '/':
-      // Peticion raiz
-      // Estableciendo cabeceras
-      res.setHeader('Content-Type', 'text/html');
-      // Escribiendo la respuesta
+      // Petición a la raíz
+      res.setHeader('Content-Type', 'text/html; charset=UTF-8');
       res.write(`
       <html>
         <head>
-          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
         </head>
         <body> 
           <h1 style="color: #333">Hello from my server</h1>
-          <p style="color: #34495E">Estas en el recurso raiz.</p>
+          <p style="color: #34495E">Estás en el recurso raíz.</p>
         </body>
       </html>
       `);
-      console.log(`📣 Respondiendo: 200 ${req.url} ${req.method}`);
-      // Estableciendo codigo de respuesta
       res.statusCode = 200;
-      // Cerrando la comunicacion
       res.end();
+      console.log(`📣 Respondiendo: 200 ${url} ${method}`);
       break;
+
     case '/author':
-      // Peticion raiz
-      // Estableciendo cabeceras
+      // Petición a la ruta "/author"
       res.setHeader('Content-Type', 'text/html');
       let url_image = 'https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=';
-      // Escribiendo la respuesta
       res.write(`
       <html>
         <head>
-          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
         </head>
         <body style="text-align: center;">
           <h1 style="color: #333;">&#9889; Author &#9889;</h1>
-          <p style="color: #34495E;">Ivan Rivalcoba Rivas - Web Developer</p>
+          <p style="color: #34495E;">Iván Rivalcoba Rivas - Web Developer</p>
           <div>
-            <img width="300px" src="https://media.istockphoto.com/id/180841365/photo/hes-a-handsome-man.jpg?s=612x612&w=0&k=20&c=vjQLLI8g_a0O6_xx0plUu3CJ9AMhnSzHssLwgem8gE4=" alt="Foto Ivan Rivalcoba">
+            <img width="300px" src="${url_image}" alt="Foto Ivan Rivalcoba">
           </div>
         </body>
       </html>
       `);
-      console.log(`📣 Respondiendo: 200 ${url} ${method}`);
-      // Estableciendo codigo de respuesta
       res.statusCode = 200;
-      // Cerrando la comunicacion
       res.end();
+      console.log(`📣 Respondiendo: 200 ${url} ${method}`);
       break;
+
+    case '/favicon.ico':
+      // Petición para el favicon
+      const faviconPath = path.join(__dirname, 'favicon.ico');
+      try {
+        const data = await fs.readFile(faviconPath);
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+        res.end(data);
+      } catch (err) {
+        console.error(err);
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+        <html>
+          <head>
+            <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
+            <title>My App</title>
+          </head>
+          <body> 
+            <h1>&#128534; 500 El servidor está fuera de servicio</h1>
+            <p>Lo sentimos, hubo un error en nuestro servidor...</p>
+            <p>${err.message}</p>
+          </body>
+        </html>
+        `);
+        res.statusCode = 500;
+        res.end();
+        console.log(`📣 Respondiendo: 500 ${url} ${method}`);
+      }
+      break;
+
     default:
-      // Peticion raiz
-      // Estableciendo cabeceras
+      // Petición a recurso no encontrado
       res.setHeader('Content-Type', 'text/html');
-      // Escribiendo la respuesta
       res.write(`
       <html>
         <head>
-          <link rel="icon" type="image/png" sizes="32x32" href="https://img.icons8.com/fluency/256/domain.png">
+          <link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon.ico">
           <title>My App</title>
         </head>
         <body> 
           <h1>&#128534; 404 Recurso no encontrado</h1>
-          <p>Lo sentimos pero no tenemos lo que buscas...</p>
+          <p>Lo sentimos, pero no tenemos lo que buscas...</p>
         </body>
       </html>
       `);
-      console.log(`📣 Respondiendo: 404 ${url} ${method}`);
-      // Estableciendo codigo de respuesta
       res.statusCode = 404;
-      // Cerrando la comunicacion
       res.end();
+      console.log(`📣 Respondiendo: 404 ${url} ${method}`);
       break;
   }
-}); 
+});
 
+// Configuración para escuchar en el puerto 3000
 server.listen(3000, "0.0.0.0", () => {
-  console.log("👩‍🍳 Servidor escuchando en http://localhost:3000"); 
+  console.log("👩‍🍳 Servidor escuchando en http://localhost:3000");
 });
